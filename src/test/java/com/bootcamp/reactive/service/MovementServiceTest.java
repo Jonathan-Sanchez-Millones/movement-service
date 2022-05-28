6160d054d4334de80dd0d6f2381edea68a80220d
Yhassuo <yguzman.20.95@gmail.com>
2022-05-28 01:25:33 -0500

    update tests

--- src/test/java/com/bootcamp/reactive/service/MovementServiceTest.java
+++ src/test/java/com/bootcamp/reactive/service/MovementServiceTest.java
@@ -1,15 +1,11 @@
 package com.bootcamp.reactive.service;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.junit4.SpringRunner;
 
